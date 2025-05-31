da57c3fabed291cf08e136a264688afc02c12572
vaibhavsindhwani <vaibhav.sindhwani@gmail.com>
2025-05-31 13:37:22 +0530

    first commit

---
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -0,0 +1,73 @@
+package testCases;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_AccountRegistrationTest extends BaseClass{
+
+	
+	@Test(groups = {"Regression", "Master"})
+	public void verify_account_registration() {
+		
+		logger.info("****Starting TC001_AccountRegistrationTest****");
+		
+		try {
+		HomePage hp = new HomePage(driver);
+		hp.clickMyAccount();
+		logger.info("Clicked on MyAccount Link");
+		
+		hp.clickRegister();
+		logger.info("Clicked on Register Link");
+		
+		AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+		
+		logger.info("Providing Customer details.....");
+		regpage.setFirstName(randomeString().toUpperCase());
+		regpage.setLastName(randomeString().toUpperCase());
+		regpage.setEmail(randomeString()+"@gmail.com");
+		regpage.setTelephone(randomeNumber());
+		
+		String password = randomAlphaNumeric();
+		
+		regpage.setPassword(password);
+		regpage.setConfirmPassword(password);
+		
+		regpage.setPrivacyPolicy();
+		regpage.clickContinue();
+		
+		logger.info("Validating expected message.....");
+		String confmsg = regpage.getConfirmationMsg();
+		
+		if(confmsg.equals("Your Account Has Been Created!")) {
+			Assert.assertTrue(true);
+		}
+		else {
+			logger.error("Test Failed....");
+			logger.debug("Debug logs...");
+			Assert.fail();
+		}
+		//Assert.assertEquals(confmsg, "Your Account Has Been Created!");	
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
+		
+		logger.info("****Finished TC001_AccountRegistrationTest****");
+	}
+	
+	
+	
+	
+	
+}
